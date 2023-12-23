221fd4a11d3bc2d84cbe220e39868cd4c1e58341
quingsley <jeromejuma25@gmail.com>
2023-12-23 08:18:46 +0300

    Update import statement for constants file

--- lib/widgets/form_fields.dart
+++ lib/widgets/form_fields.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:inter_intel_interview_test/providers/providers.dart';
-import 'package:inter_intel_interview_test/utils/constants.dart';
+import 'package:inter_intel_interview_test/constants/constants.dart';
 import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 class FormFields extends ConsumerWidget {
