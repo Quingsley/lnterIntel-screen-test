761f006bbf60d8ff363ec426fc951fff93db4ce2
quingsley <jeromejuma25@gmail.com>
2023-12-23 08:19:59 +0300

    Refactor VariantCheckBoxText to use ConsumerWidget

--- lib/widgets/options_cta.dart
+++ lib/widgets/options_cta.dart
@@ -1,27 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:inter_intel_interview_test/providers/providers.dart';
 
-class VariantCheckBoxText extends StatefulWidget {
+class VariantCheckBoxText extends ConsumerWidget {
   const VariantCheckBoxText({super.key});
 
   @override
-  State<VariantCheckBoxText> createState() => _VariantCheckBoxTextState();
-}
-
-class _VariantCheckBoxTextState extends State<VariantCheckBoxText> {
-  bool _isChecked = false;
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var isChecked = ref.watch(isCheckedState);
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Expanded(
           child: Checkbox(
               activeColor: Theme.of(context).colorScheme.inversePrimary,
-              value: _isChecked,
+              value: isChecked,
               onChanged: (value) {
-                setState(() {
-                  _isChecked = value!;
-                });
+                ref.read(isCheckedState.notifier).state = value!;
               }),
         ),
         const Expanded(
