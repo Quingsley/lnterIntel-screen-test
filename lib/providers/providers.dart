2db318523a19829850038d7ebd507d38882dc1e5
quingsley <jeromejuma25@gmail.com>
2023-12-23 08:16:18 +0300

    Update imports and refactor state providers in providers.dart

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,29 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:inter_intel_interview_test/utils/constants.dart';
+import 'package:inter_intel_interview_test/constants/constants.dart';
+import 'package:inter_intel_interview_test/providers/generated_combinations_provider.dart';
+import 'package:inter_intel_interview_test/providers/user_selected_combinations_provder.dart';
 
 // using state provider to keep things simple
 final selectedColors = StateProvider<List<Color?>>((ref) => []);
 final selectedSizes = StateProvider<List<ShirtSize?>>((ref) => []);
-final userSelectedCombinations = StateProvider<List<String>>((ref) => []);
+final isCheckedState = StateProvider<bool>((ref) => false);
 final combinations =
     NotifierProvider<Combinations, List<String>>(() => Combinations());
 
-class Combinations extends Notifier<List<String>> {
-  @override
-  List<String> build() {
-    return [];
-  }
-
-//logic to generate combinations
-  void combinations(List<Color?> colors, List<ShirtSize?> sizes) {
-    state = colors
-        .expand((colors) => sizes
-            .map((size) => '${getSizeName(size!)} / ${getColorName(colors!)}'))
-        .toList();
-  }
-
-  void removeCombination(String value) {
-    state = state.where((c) => c != value).toList();
-  }
-}
+final userSelectedCombinations =
+    NotifierProvider<UserSelectedCombinationsNotifier, List<String>>(
+        () => UserSelectedCombinationsNotifier());
