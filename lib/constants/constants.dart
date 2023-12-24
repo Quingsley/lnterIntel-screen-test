314b38a57260dfe336fb3df67030c1927f23ee33
quingsley <jeromejuma25@gmail.com>
2023-12-24 10:48:08 +0300

    Update color and size names

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// color options
 final List<Color> colors = [
   Colors.green,
   Colors.blue,
@@ -7,14 +8,16 @@ final List<Color> colors = [
 
 enum ShirtSize { small, medium, large, extraLarge, extraExtraLarge }
 
+// method to get the color name
 String getColorName(Color c) => c == Colors.blue ? 'blue' : 'green';
 
+// method to get the size name
 String getSizeName(ShirtSize s) {
   return switch (s) {
-    ShirtSize.small => 'small',
-    ShirtSize.medium => 'medium',
+    ShirtSize.small => 'Small',
+    ShirtSize.medium => 'Medium',
     ShirtSize.large => 'large',
-    ShirtSize.extraLarge => 'extra large',
-    ShirtSize.extraExtraLarge => 'extra extra large',
+    ShirtSize.extraLarge => 'Extra-large',
+    ShirtSize.extraExtraLarge => 'Extra-extra-large',
   };
 }
